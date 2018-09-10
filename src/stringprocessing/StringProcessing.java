acfaf4a0ec455ce40d30fa4a3c687504e3348b13
Leyu Gao <lgao1@aggies.ncat.edu>
2018-09-10 17:14:23 -0400

    test

--- src/stringprocessing/StringProcessing.java
+++ src/stringprocessing/StringProcessing.java
@@ -1,50 +1,156 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package stringprocessing;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  *
- * @author Edgeside
+ * @author lgao1
  */
 public class StringProcessing {
-    private String[] records;
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
+
         String[] records = new String[100];
-        try{
+
+        try {
             Scanner reader = new Scanner(new File(args[0]));
-        
             int currentIndex = 0;
-            while(reader.hasNext()){
+
+            while (reader.hasNext()) {
+
                 String[] line = reader.nextLine().split(",");
+
                 String firstName = line[0];
                 String lastName = line[1];
-                String age = line[2];
-                String gender = line[3];
+                String gender = line[2];
+                String age = line[3];
                 String phoneNumber = line[4];
-                String email = line[5];       
-                
-                records[currentIndex] = String.format("%-20s %-20s %-10s %-10s %-20s %-20s", firstName, lastName, age, gender, phoneNumber, email);
-                currentIndex ++;
-            
+                String email = line[5];
+
+                if (!validateName(firstName) || !validateName(lastName)) {
+                    continue;
+                }
+                if (!validateGender(gender)) {
+                    continue;
+                }
+                if (!validateAge(age)) {
+                    continue;
+                }
+                if (!validatePhoneNumber(phoneNumber)) {
+                    continue;
+                }
+                if (!validateEmail(email)) {
+                    continue;
+                }
+
+                records[currentIndex] = String.format("%-20s%-20s%-10s%-4s%-15s%-20s", firstName, lastName, gender, age, phoneNumber, email);
+                currentIndex++;
+
             }
-        
-        } catch(FileNotFoundException ex){
+
+        } catch (FileNotFoundException ex) {
             System.err.println("Could not find input file");
-       
         }
-        
-        for(int i = 0; i < records.length; i++){
+
+        for (int i = 0; i < records.length; i++) {
             System.out.println(records[i]);
         }
+
+        return;
+
     }
-    
+
+    public static boolean validateName(String name) {
+
+        char[] nameArr = name.toCharArray();
+
+        for (int i = 0; i < name.length(); i++) {
+            if (!Character.isAlphabetic(nameArr[i]) && nameArr[i] != ' ') {
+                System.out.println("ERROR: Invalid name: " + name);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean validateGender(String gender) {
+
+        gender = gender.replaceAll(" ", ""); //gender has a space in the front of it when read.
+
+        if (gender.equalsIgnoreCase("male") || gender.equalsIgnoreCase("female")) {
+            return true;
+        } else {
+            System.out.println("ERROR: Invalid gender: " + gender);
+            return false;
+        }
+    }
+
+    public static boolean validateAge(String age) {
+
+        if (age.equals("")) {
+            System.out.println("ERROR: Invalid age: " + age);
+            return false;
+        }
+        int ageInt = Integer.parseInt(age);
+
+        if (1 <= ageInt && ageInt <= 130) {
+            return true;
+        }
+
+        System.out.println("ERROR: Invalid age: " + age);
+        return false;
+
+    }
+
+    //TODO: CHECK THIS METHOD
+    public static boolean validatePhoneNumber(String phoneNumber) {
+
+        char[] phoneNumberArr = phoneNumber.toCharArray();
+        int countDigits = 0;
+
+        for (int i = 0; i < phoneNumberArr.length; i++) {
+            if (Character.isDigit(phoneNumberArr[i])) {
+                countDigits += 1;
+            }
+        }
+
+        if (countDigits != 10) {
+            System.out.println("ERROR: Invalid phonenumber " + phoneNumber);
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean validateEmail(String email) {
+
+        char[] emailArr = email.toCharArray();
+        boolean containsAtSymbol = false;
+
+        if (!Character.isLetter(emailArr[0])) {
+            System.out.println("ERROR: Invalid email: " + email);
+            return false;
+        }
+
+        for (int i = 0; i < emailArr.length; i++) {
+
+            if (emailArr[i] == '@') {
+                containsAtSymbol = true;
+                continue;
+            }
+
+            if (!Character.isLetterOrDigit(emailArr[i]) && emailArr[i] != '.') {
+                System.out.println("ERROR: Invalid email: " + email);
+                return false;
+            } else if (emailArr[i] == '.') {
+                continue;
+            }
+        }
+
+        return true;
+    }
 }
